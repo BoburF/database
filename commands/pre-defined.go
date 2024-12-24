8f4843c2e9f8c3734273b37ed76f84e5126c8ea7
BoburF <changeitgo3@gmail.com>
2024-12-25 00:15:09 +0500

    [*] client is created

--- commands/pre-defined.go
+++ commands/pre-defined.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/BoburF/database/protocol"
@@ -19,6 +20,24 @@ func RegisterPredefinedCommands(server *protocol.Server) {
 
 	server.RegisterCommand("QUIT", func(conn net.Conn) error {
 		_, err := conn.Write([]byte("Goodbye\n"))
+		conn.Close()
+		return err
+	})
+}
+
+func RegisterPredefinedClientCommands(client *protocol.Client) {
+	client.RegisterCommand("PING", func(args string, conn net.Conn) error {
+		_, err := conn.Write([]byte(fmt.Sprintf("%02dPING\x00%s", 4, args)))
+		return err
+	})
+
+	client.RegisterCommand("ECHO", func(args string, conn net.Conn) error {
+		_, err := conn.Write([]byte(fmt.Sprintf("%02dECHO\x00%s", 4, args)))
+		return err
+	})
+
+	client.RegisterCommand("QUIT", func(args string, conn net.Conn) error {
+		_, err := conn.Write([]byte(fmt.Sprintf("%02dQUIT\x00%s", 4, args)))
 		conn.Close()
 		return err
 	})
--- protocol/client.go
+++ protocol/client.go
@@ -1,7 +1,42 @@
 package protocol
 
+import (
+	"errors"
+	"fmt"
+	"net"
+	"strings"
+)
+
 type Client struct {
-	commmands map[string]ClientCommand
+	conn     net.Conn
+	commands map[string]ClientCommand
 }
 
+func (c *Client) NewConnect(host string, port int) error {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		return err
+	}
+	c.conn = conn
+	c.commands = make(map[string]ClientCommand)
 
+	return nil
+}
+
+func (c *Client) Call(command string, args string) error {
+	handler, exists := c.commands[command]
+	if !exists {
+		return errors.New("Command is not defined")
+	}
+
+	handler.Handler(args, c.conn)
+
+	return nil
+}
+
+func (c *Client) RegisterCommand(name string, handler func(args string, conn net.Conn) error) {
+	c.commands[name] = ClientCommand{
+		Name:    strings.ToUpper(name),
+		Handler: handler,
+	}
+}
